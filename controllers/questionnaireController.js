e57fe900d3cdb5d25db702a108842645f5ca0aad
S_Stoykov26 <thelifeishere@abv.bg>
2024-05-30 16:32:23 +0300

    update questions api

--- controllers/questionnaireController.js
+++ controllers/questionnaireController.js
@@ -14,4 +14,16 @@ router.get('/getAllQuestionnaires', async (req, res) => {
     }
   });
 
+router.get('/getQuestionnaireById', async (req, res) => {
+  try {
+    const questionnaireId = req.query.questionnaireId;
+    const result = await pool.query('SELECT "questionnaire_id", "questionnaire_type"FROM "OINS_SS".questionnaire WHERE "questionnaire_id" = $1',
+    [questionnaireId]);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
