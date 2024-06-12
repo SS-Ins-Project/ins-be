06d9f8bc2d5a38b6cde76eb07ddd6db91d3a429c
S_Stoykov26 <thelifeishere@abv.bg>
2024-06-13 01:35:49 +0300

    https security protocol

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 443;
+const port = 443;
 const pool = require('./db');
+
+const options = {
+    key: fs.readFileSync('C:/Users/stasi/key.pem'),
+    cert: fs.readFileSync('C:/Users/stasi/cert.pem')
+  };
 
 var questionnaireController = require('./controllers/questionnaireController');
 var questionsController = require('./controllers/questionsController');
@@ -10,9 +17,24 @@ var optionsController = require('./controllers/optionsController');
 var tariffController = require('./controllers/tariffController');
 
 app.use(cors());
-app.listen(PORT, () => {
-    console.log(`Server is running on https://localhost:${PORT}`);
-});
+app.use(express.json());
+// Start HTTPS server
+https.createServer(options, app).listen(port, () => {
+    console.log(`Secure server running on port ${port}`);
+  });
+
+  const http = express();
+  http.use((req, res, next) => {
+    if (!req.secure) {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+  });
+  
+  const httpPort = 80;
+  http.listen(httpPort, () => {
+    console.log(`HTTP server running on port ${httpPort}, redirecting to HTTPS`);
+  });
 
 app.use('/api/questionnaire', questionnaireController);
 app.use('/api/questions', questionsController);
